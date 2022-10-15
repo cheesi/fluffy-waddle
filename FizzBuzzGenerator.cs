dc62adce9a53a4f35acbe905ee3b77e875badf0f
Christian Schabetsberger <christian.Schabetsberger@avocodo.com>
2022-10-15 16:15:45 +0200

    Add project files.

---
+++ FizzBuzzGenerator.cs
@@ -0,0 +1,24 @@
+﻿namespace FizzBuzzRefucktoring;
+
+public static class FizzBuzzGenerator
+{
+    public static string Generate(int number)
+    {
+        if (number % 3 == 0 && number % 5 == 0)
+        {
+            return "FizzBuzz";
+        }
+        else if (number % 3 == 0)
+        {
+            return "Fizz";
+        }
+        else if (number % 5 == 0)
+        {
+            return "Buzz";
+        }
+        else
+        {
+            return number.ToString();
+        }
+    }
+}
